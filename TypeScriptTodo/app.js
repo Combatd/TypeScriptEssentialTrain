e6a6460debcf7e59c2044150aa4c94986a61db8c
Combatd <combatdstar@gmail.com>
2021-03-23 10:19:01 -0700

    Declare enum TodoState

--- TypeScriptTodo/app.js
+++ TypeScriptTodo/app.js
@@ -1,3 +1,22 @@
+// interface ITodoService {
+//     add(todo: Todo): Todo;
+//     delete(todoId: number): void;
+//     getAll(): Todo[];
+//     getById(todoId: number): Todo;
+// }
+var TodoState;
+(function (TodoState) {
+    TodoState[TodoState["New"] = 1] = "New";
+    TodoState[TodoState["Active"] = 2] = "Active";
+    TodoState[TodoState["Complete"] = 3] = "Complete";
+    TodoState[TodoState["Deleted"] = 4] = "Deleted";
+})(TodoState || (TodoState = {}));
 var todo = {
-    name: 'PIck up drycleaning'
+    name: 'PIck up drycleaning',
+    state: TodoState.New
 };
+function deleteTodo(todo) {
+    if (todo.state != TodoState.Complete) {
+        throw "Can't delete incomplete task";
+    }
+}
--- TypeScriptTodo/app.ts
+++ TypeScriptTodo/app.ts
@@ -1,15 +1,30 @@
 interface Todo {
     name: string;
-    completed?: boolean; // completed property is optional, but when it exists, always a boolean
+    state: TodoState;
 }
 
-interface ITodoService {
-    add(todo: Todo): Todo;
-    delete(todoId: number): void;
-    getAll(): Todo[];
-    getById(todoId: number): Todo;
+// interface ITodoService {
+//     add(todo: Todo): Todo;
+//     delete(todoId: number): void;
+//     getAll(): Todo[];
+//     getById(todoId: number): Todo;
+// }
+
+enum TodoState {
+    New = 1,
+    Active = 2,
+    Complete = 3,
+    Deleted = 4
 }
 
 let todo: Todo = {
-    name: 'PIck up drycleaning'
+    name: 'PIck up drycleaning',
+    state: TodoState.New
 };
+
+
+function deleteTodo (todo: Todo) {
+    if (todo.state != TodoState.Complete) {
+        throw "Can't delete incomplete task"
+    }
+}
