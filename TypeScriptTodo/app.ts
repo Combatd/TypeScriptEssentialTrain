9d172076cb3e0994077b883d1dfe895c31d844b7
Combatd <combatdstar@gmail.com>
2021-03-24 01:04:58 -0700

    Define #totalLength with Generic type constraint

--- TypeScriptTodo/app.ts
+++ TypeScriptTodo/app.ts
@@ -126,3 +126,16 @@ class KeyValuePairPrinter <T, U> {
 
 // pair2 doesn't share the same generic type parameters, so it is a different type of object
 // let printer = new KeyValuePairPrinter([pair, pair2, pair3]);
+
+interface IHaveALength {
+    length: number;
+}
+
+function totalLength<T extends IHaveALength>(x: T, y: T) {
+    var total: number = x.length + y.length;
+    return total;
+}
+
+class CustomArray<T> extends Array<T> {}
+
+var length = totalLength([1, 2, 3], new CustomArray<number>())
